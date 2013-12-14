0729be2d4e35dab9b57b4ed8aa06c0a6d9ab2fe3
RouLeTaMiX <rouletamix@gmail.com>
2013-12-14 20:46:50 +0100

    Add : new model.

--- mini_url/models.py
+++ mini_url/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+
+class myURL(models.Model):
+    url = models.URLField(verbose_name='Your base url', unique=True)
+    secret = models.CharField(verbose_name='My Secret Key', max_length=10, unique=True)
+    username = models.CharField(verbose_name='Nickname', max_length=255, blank=True, null=True)
+    accessNb = models.IntegerField(default=0, verbose_name='Number of access allowed')
+    date = models.DateTimeField(verbose_name='Added', auto_now_add=True)
+
+    class Meta:
+        verbose_name="URL Reductor"
