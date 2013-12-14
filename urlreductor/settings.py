a0c8cd69495bd801849bc17fc838c2f28acd7aad
RouLeTaMiX <rouletamix@gmail.com>
2013-12-14 21:59:20 +0100

    Fix: Fix some error during the launch of the dev server, well now i need to play doto with R3D2

--- urlreductor/settings.py
+++ urlreductor/settings.py
@@ -82,3 +82,17 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.6/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+    # Put strings here, like "/home/html/static" or "C:/www/django/static".
+    # Always use forward slashes, even on Windows.
+    # Don't forget to use absolute paths, not relative paths.
+)
+
+TEMPLATE_DIRS = (
+    os.path.join(BASE_DIR, 'templates'),
+    # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
+    # Always use forward slashes, even on Windows.
+    # Don't forget to use absolute paths, not relative paths.
+)
