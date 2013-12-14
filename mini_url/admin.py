35b49de5c43646fe2d9143f1f6ce72626cdcc90d
RouLeTaMiX <rouletamix@gmail.com>
2013-12-14 21:42:18 +0100

    Fix: an error when i call the model.ModelAdmin

--- mini_url/admin.py
+++ mini_url/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from models import myURL
 
 
-class myURLAdmin(admin.Admins):
+class myURLAdmin(admin.ModelAdmin):
     list_display = ('url', 'username', 'date', 'secret', 'accessNb', 'date')
     list_filter = ('pseudo', )
     date_hierarchy = 'date'
