1c3036896fe69674dd3b7570e095e1686486d77f
RouLeTaMiX <rouletamix@gmail.com>
2013-12-14 21:09:30 +0100

    Add : admin management and set some option.

--- mini_url/admin.py
+++ mini_url/admin.py
@@ -4,3 +4,10 @@ from models import myURL
 
 class myURLAdmin(admin.Admins):
     list_display = ('url', 'username', 'date', 'secret', 'accessNb', 'date')
+    list_filter = ('pseudo', )
+    date_hierarchy = 'date'
+    ordering = ('date', )
+    search_fields = ('url',)
+
+
+admin.site.register(myURL, myURLAdmin)
