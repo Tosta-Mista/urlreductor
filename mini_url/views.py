57affea8d5c083c2e7c516b07406e0b69b4cda67
RouLeTaMiX <rouletamix@gmail.com>
2013-12-14 21:33:07 +0100

    Add: Bootstrap3 CSS on view.

--- mini_url/views.py
+++ mini_url/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+# -*- coding: utf-8 -*-
+from django.shortcuts import render, redirect
 
-# Create your views here.
+
+def list(request):
+    """Display list of redirection"""
+    urls = myURL.objects.order_by('-accessNb')
+    return render(request, 'mini_url/list.html')
+
+
+
