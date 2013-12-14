5a90c875219f234ce9132e5993786bbf737ee1f2
RouLeTaMiX <rouletamix@gmail.com>
2013-12-14 21:17:00 +0100

    Add : url import to project, and create new url file to my app.

---
+++ mini_url/urls.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+from django.conf.urls import patterns, url
+
+
+urlpatterns = patterns('mini_url.views',
+        url(r'^$', 'list', name='url_list'),
+        url(r'^new/$', 'new', name='new_url'),
+        url(r'^(?P<secret>\w{10})/$', 'redirection', 'url_redirection'),
+)
--- urlreductor/urls.py
+++ urlreductor/urls.py
@@ -4,6 +4,7 @@ from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
+    url(r'^urlfactory/', include('mini_url.urls')),
     # Examples:
     # url(r'^$', 'urlreductor.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
