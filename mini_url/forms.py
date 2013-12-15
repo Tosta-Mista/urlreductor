95a14dfcc94ddb496e72dadd44a8fa4002fdf057
RouLeTaMiX <rouletamix@gmail.com>
2013-12-15 15:22:59 +0100

    Fix : a lot of errors now the app work well and it's finished.

--- mini_url/forms.py
+++ mini_url/forms.py
@@ -3,7 +3,7 @@ from django import forms
 from models import myURL
 
 
-class myURLForm(forms.Form):
+class myURLForm(forms.ModelForm):
     class Meta:
         model = myURL
         fields = ('url', 'username')
--- mini_url/models.py
+++ mini_url/models.py
@@ -6,9 +6,9 @@ import string
 
 class myURL(models.Model):
     url = models.URLField(verbose_name='Your base url', unique=True)
-    secret = models.CharField(verbose_name='My Secret Key', max_length=10, unique=True)
+    secret = models.CharField(verbose_name='My Secret Key', max_length=6, unique=True)
     username = models.CharField(verbose_name='Nickname', max_length=255, blank=True, null=True)
-    accessNb = models.IntegerField(default=0, verbose_name='Number of access allowed')
+    accessNb = models.FloatField(default=0, verbose_name='Number of access allowed')
     date = models.DateTimeField(verbose_name='Added', auto_now_add=True)
 
     def __unicode__(self):
@@ -16,14 +16,14 @@ class myURL(models.Model):
 
     def save(self, *args, **kwargs):
         if self.pk is None:
-            self.generate(10)
+            self.generate(6)
 
         super(myURL, self).save(*args, **kwargs)
 
     def generate(self, N):
         char = string.letters + string.digits
         randomize = [random.choice(char) for _ in xrange(N)]
-        self.accessNb = ''.join(randomize)
+        self.secret = ''.join(randomize)
 
     class Meta:
         verbose_name="URL Reductor"
