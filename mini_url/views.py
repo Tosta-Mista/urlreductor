f956cf3472fc8816d7ee84e28f4afc88250ff390
RouLeTaMiX <rouletamix@gmail.com>
2013-12-15 13:52:01 +0100

    Add: New form to add a new url.

--- mini_url/views.py
+++ mini_url/views.py
@@ -1,12 +1,26 @@
 # -*- coding: utf-8 -*-
 from django.shortcuts import render, redirect
 from models import myURL
+from forms import myURLForm
 
 
 def list(request):
     """Display list of redirection"""
     urls = myURL.objects.order_by('-accessNb')
-    return render(request, 'mini_url/list.html')
+    return render(request, 'mini_url/list.html', locals())
+
+
+def new(request):
+    """Add a new redirection"""
+    if request.method == "POST":
+        form = myURLForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect(list)
+    else:
+        form = myURLForm()
+
+    return render(request, mini_url/new.html, {'form': form})
 
 
 
